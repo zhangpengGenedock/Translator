a71823d5718d1b309145450c05d219db7778fc70
zhangpeng <zhangpengGenedock@genedock.com>
2017-09-24 05:57:57 +0000

    MOD: fix length_penalty_weight parameter 1 to 0

--- actual_run.sh
+++ actual_run.sh
@@ -3,9 +3,10 @@ python -m nmt.nmt \
     --hparams_path=nmt/standard_hparams/wmt16_gnmt_4_layer.json \
     --train_prefix=actual_data/train_seg \
     --dev_prefix=actual_data/dev_seg \
+    --test_prefix=actual_data/dev_seg \
     --vocab_prefix=actual_data/vocab \
+    --num_gpus=1 \
     --out_dir=model 
-   # --test_prefix=actual_data/dev_seg \
    # --num_gpus=2 \
    # --num_train_steps=12000 \
    # --steps_per_stats=100 \

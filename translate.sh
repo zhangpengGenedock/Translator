19ad25f996052838c1fac05fd7fc9219cd7b0199
eic <zhangpeng@fenbi.com>
2017-10-08 20:46:07 +0800

    MOD: install nvidia driver and wmt config

--- translate.sh
+++ translate.sh
@@ -1,4 +1,4 @@
 python -m nmt.nmt \
-    --out_dir=model/ \
+    --out_dir=stf_model/ \
     --inference_input_file=ai_data/ai_challenger_translation_test_a_20170923.en_seg \
     --inference_output_file=ai_data/ai_challenger_translation_test_a_20170923.zh_seg
--- waya-dl-setup.sh
+++ waya-dl-setup.sh
@@ -6,16 +6,16 @@ CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
 sudo dpkg -i ${CUDA_REPO_PKG}
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda-8-0
 
 # install cuDNN v6.0
 CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
 wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
 tar -xzvf ${CUDNN_TAR_FILE}
-sudo cp -P cuda/include/cudnn.h /usr/local/cuda-9.0/include
-sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-9.0/lib64/
-sudo chmod a+r /usr/local/cuda-9.0/lib64/libcudnn*
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
+sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
 
 # set environment variables
-export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
